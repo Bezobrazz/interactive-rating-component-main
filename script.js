8bc7c79cd3b47efdb29134286c3f20e95f0e7583
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2022-12-14 12:49:14 +0200

    Final

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const ratingContainer = document.querySelector(".card__numbers");
 console.log(ratingContainer);
 const ratingNumbers = document.querySelectorAll(".card__number");
 console.log(ratingNumbers);
+const submitBtn = document.querySelector(".card__btn");
+const thankYouCard = document.querySelector(".thankyou__wrapper");
+const thankYouSelectedHml = document.querySelector(".thankyou__selected-text");
 
 // ratingNumbers.forEach((number) =>
 //   number.addEventListener("click", function () {
@@ -21,8 +24,15 @@ ratingNumbers.forEach((number) =>
     });
     number.addEventListener("click", function () {
       console.log(number.id);
+      thankYouSelectedHml.innerHTML = `
+		<p class="thankyou__selected-text">You selected ${number.id} of 5</p>`;
       ratingContainer.querySelector(".active").classList.remove("active");
       number.classList.add("active");
     });
   })
 );
+
+submitBtn.addEventListener("click", function () {
+  thankYouCard.style.display = "block";
+  console.log(thankYouCard);
+});
